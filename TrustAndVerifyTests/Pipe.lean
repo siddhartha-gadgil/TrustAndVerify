e85851dcc70690b6dc941c2d2331eaf0df2e2002
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2026-08-18 11:50:01 +0530

    pipe, untested

---
+++ TrustAndVerifyTests/Pipe.lean
@@ -0,0 +1,38 @@
+import Lean
+
+open Lean
+
+namespace TrustAndVerify
+
+/--
+A connection to a LeanAide server, which can be used to query for responses.
+-/
+class Pipe (X Y : Type) where
+  queryResponse : X → IO Y
+
+class LeanAideUrl where
+  url : String
+
+/-- Create a `Pipe` from a URL, using `curl` to send requests. -/
+@[instance_reducible]
+def fromURL (url: String) : Pipe Json Json := {
+  queryResponse (data: Json) := do
+    let output ← IO.Process.run {cmd := "curl", args := #[url, "-X", "POST", "-H", "Content-Type: application/json", "--data", data.compress]}
+    let .ok response :=
+      Json.parse output | IO.throwServerError s!"Failed to parse response: \n{output}"
+    return response
+}
+
+macro (name := pipe) "#pipe" url:str : command => do
+  let jsonId := mkIdent ``Json
+  let pipeId := mkIdent ``Pipe
+  `(command| instance : $pipeId $jsonId $jsonId := $url)
+
+def runPipe {X Y α β: Type} [pipe: Pipe X Y][Inhabited β] (encode : α → X) (decode : Y → β) (input : α) : β  :=
+  let x := encode input
+  let y? := unsafe unsafeIO <| pipe.queryResponse x
+  match y? with
+    | .ok y => decode y
+    | .error e => panic! s!"Error in runPipe: {e}"
+
+end TrustAndVerify
